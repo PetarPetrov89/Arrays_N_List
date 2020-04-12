1bdaa53f276cd1c9c553628653e69f2a877ee5cf
PetarPetrov89 <62606018+PetarPetrov89@users.noreply.github.com>
2020-04-12 19:07:03 +0300

    Exercices.

---
+++ Array_7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Моля въведете число");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            Console.WriteLine("Въведете стойност на масива");
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            bool symmetric = true;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (array[i] != array[n - 1- i])
+                {
+                    symmetric = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"Симетричен ли е масива? {symmetric}");
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>() { 1,3,5,6,7};
+            
+            numberList.Add(2);
+            numberList.Remove(3);
+            Console.WriteLine(numberList.IndexOf(22));
+
+        }
+    }
+}
